44fda8cee57da9e870aeaf39cf1e9ffa619e5c32
chayanon12 <chayanonolo@gmail.com>
2023-08-21 15:15:05 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace employeeDatabaseStorage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.lblSucess = new System.Windows.Forms.Label();
+            this.lblError = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtLastname = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(12, 12);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(268, 93);
+            this.btnCheck.TabIndex = 0;
+            this.btnCheck.Text = "button1";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // lblSucess
+            // 
+            this.lblSucess.AutoSize = true;
+            this.lblSucess.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSucess.ForeColor = System.Drawing.Color.Lime;
+            this.lblSucess.Location = new System.Drawing.Point(12, 199);
+            this.lblSucess.Name = "lblSucess";
+            this.lblSucess.Size = new System.Drawing.Size(105, 29);
+            this.lblSucess.TabIndex = 1;
+            this.lblSucess.Text = "Sucess!";
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblError.ForeColor = System.Drawing.Color.Red;
+            this.lblError.Location = new System.Drawing.Point(7, 199);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(220, 29);
+            this.lblError.TabIndex = 2;
+            this.lblError.Text = "Error to connect!!!";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(461, 27);
+            this.txtName.Multiline = true;
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(306, 56);
+            this.txtName.TabIndex = 3;
+            // 
+            // txtLastname
+            // 
+            this.txtLastname.Location = new System.Drawing.Point(461, 102);
+            this.txtLastname.Multiline = true;
+            this.txtLastname.Name = "txtLastname";
+            this.txtLastname.Size = new System.Drawing.Size(306, 56);
+            this.txtLastname.TabIndex = 4;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(552, 182);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(140, 70);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(325, 27);
+            this.txtId.Multiline = true;
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(109, 56);
+            this.txtId.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtLastname);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.lblSucess);
+            this.Controls.Add(this.btnCheck);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Connection MySQL";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Label lblSucess;
+        private System.Windows.Forms.Label lblError;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtLastname;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtId;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,90 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace employeeDatabaseStorage
+{
+    public partial class Form1 : Form
+    {
+        Data.Connection conn = new Data.Connection();
+        public Form1()
+        {
+            InitializeComponent();
+            lblSucess.Visible = false;
+            lblError.Visible = false;
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Data.Connection.dataSource();
+                conn.connOpen();
+                lblSucess.Visible = true;
+                lblError.Visible = false;
+                conn.connClose();
+            }
+            catch (Exception)
+            { 
+                lblError.Visible = true;
+                lblSucess.Visible = false;  
+                conn.connClose();
+            }
+            finally
+            {
+               
+                conn.connClose ();
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string name = txtName.Text;
+                string lastName = txtLastname.Text;
+
+                if (string.IsNullOrWhiteSpace(name) || string.IsNullOrWhiteSpace(lastName))
+                {
+                    MessageBox.Show("Both name and last name are required.");
+                    return;
+                }
+
+                using (MySqlConnection connection = Data.Connection.dataSource())
+                {
+                    connection.Open();
+
+                    string insertQuery = "INSERT INTO employees (Name, LastName) VALUES (@name, @lastName)";
+                    using (MySqlCommand command = new MySqlCommand(insertQuery, connection))
+                    {
+                        command.Parameters.AddWithValue("@name", name);
+                        command.Parameters.AddWithValue("@lastName", lastName);
+
+                        int rowsAffected = command.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show("Data added successfully.");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Data insertion failed.");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message);
+            }
+        }
+
+    }
+}
